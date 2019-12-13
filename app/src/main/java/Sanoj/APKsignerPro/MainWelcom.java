2d996f7723b3b0f2b93b3ae86676527cab44adfd
00sanoj00 <00sanoj00@gmail.com>
2019-12-14 00:27:07 +0530

    modify file manager fix build error add new layout support android 10

--- app/src/main/java/Sanoj/APKsignerPro/MainWelcom.java
+++ app/src/main/java/Sanoj/APKsignerPro/MainWelcom.java
@@ -2,7 +2,10 @@ package Sanoj.APKsignerPro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainWelcom extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainWelcom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_welcom);
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+
+                Intent mainIntent = new Intent(MainWelcom.this, ZipPickerActivity.class);
+                MainWelcom.this.startActivity(mainIntent);
+                MainWelcom.this.finish();
+
+
+
+            }
+        }, 5000);
+
+
     }
 }
